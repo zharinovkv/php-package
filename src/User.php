742085a61657b358f7dae00deedd2c8ab1c95b18
Kirill Mokevnin <mokevnin@gmail.com>
2018-01-04 17:11:09 +0300

    add collect

--- src/User.php
+++ src/User.php
@@ -2,16 +2,24 @@
 
 namespace Php\Package;
 
+use Illuminate\Support\Collection;
+
 class User
 {
     private $name;
 
-    public function __construct($name)
+    public function __construct($name, $children = [])
     {
         $this->name = $name;
+        $this->children = collect($children);
     }
 
     public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getChildren()
     {
         return $this->name;
     }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -10,7 +10,7 @@ class UserTest extends TestCase
     public function testGetName()
     {
         $name = 'john';
-        $user = new User($name);
+        $user = new User($name, [new User('Mark')]);
 
         $this->assertEquals($name, $user->getName());
     }
